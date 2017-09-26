eca90a0c47e943cd4e55616d59f44a73da13883e
Konrad <konradklimczak93@gmail.com>
2017-09-26 21:38:38 +0200

    connect to WI FI

--- init.lua
+++ init.lua
@@ -1,22 +1,30 @@
--- A simple Hello World Lua Script
-print('Hello World!')
+--load credentials
 
-print('|---|')
-print('| H |')
-print('| E |')
-print('| L |')
-print('| L |')
-print('| O |')
-print('| | |')
-print('| W |')
-print('| O |')
-print('| R |')
-print('| L |')
-print('| D |')
-print('|---|')
+function startup()
+  if file.open("init.lua") == nil then
+    print("init.lua deleted")
+  else
+    print("Running")
+    file.close("init.lua")
+    -- dofile("sensor.lua")
+  end
+end
 
-print('YEAH!!! HELLO WORLD!!!')
+--init.lua
+print("Setting up wifi")
+wifi.setmode(wifi.STATION)
 
-local l = [==[Lorem ipsum dolor sit amet, consetetur sadipscing elitr]==]
-
-print('String: '..l)
+wifi.sta.config(station_cfg)
+wifi.sta.connect()
+wifi.sta.autoconnect(1)
+tmr.alarm(1, 1000, 1, function()
+    if wifi.sta.getip()== nil then
+      print("Waiting on dhcp...")
+    else
+      tmr.stop(1)
+      print("Config done, IP is "..wifi.sta.getip())
+      print("You have 5 seconds to abort startup")
+      print("Waiting...")
+      tmr.alarm(0, 5000, 0, startup)
+    end
+ end)

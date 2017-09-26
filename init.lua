6a578a8a8aad3efbad6b4c2c5a1e5925328ca9bc
Konrad <konradklimczak93@gmail.com>
2017-09-26 22:05:37 +0200

    Read pin value. Gues what. It doesn't work.

--- init.lua
+++ init.lua
@@ -1,30 +1,15 @@
---load credentials
+print("PIN TEST");
 
-function startup()
-  if file.open("init.lua") == nil then
-    print("init.lua deleted")
-  else
-    print("Running")
-    file.close("init.lua")
-    -- dofile("sensor.lua")
-  end
+print("SETTING UP PIN 1 - this is the one I use probably");
+gpio.mode(1, gpio.INT, gpio.PULLUP)
+
+print("DEFINE HANDLER");
+function onChange (level)
+    print("LEVEL"..level);
+    print("PIN IS GETING SIGNAL");
+    print("IT WORKS");
 end
 
---init.lua
-print("Setting up wifi")
-wifi.setmode(wifi.STATION)
-
-wifi.sta.config(station_cfg)
-wifi.sta.connect()
-wifi.sta.autoconnect(1)
-tmr.alarm(1, 1000, 1, function()
-    if wifi.sta.getip()== nil then
-      print("Waiting on dhcp...")
-    else
-      tmr.stop(1)
-      print("Config done, IP is "..wifi.sta.getip())
-      print("You have 5 seconds to abort startup")
-      print("Waiting...")
-      tmr.alarm(0, 5000, 0, startup)
-    end
- end)
+print("ATTACH HANDLER");
+gpio.trig(1, "both", onChange);
+print("HANDLER ATTACHED");

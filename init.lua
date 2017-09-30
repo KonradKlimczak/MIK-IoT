d557f1fa5c0ad9f2b6a1421b1878e96e56b4862b
Konrad <konradklimczak93@gmail.com>
2017-09-30 15:27:09 +0200

    sweet

--- init.lua
+++ init.lua
@@ -1,11 +1,31 @@
 print("PIN TEST");
 
+-- shamelessly adapted from github https://gist.github.com/marcelstoer/59563e791effa4acb65f
+function debounce (func)
+    local last = 0
+    local delay = 1000 * 1000 -- 1000ms * 1000 as tmr.now() has μs resolution
+
+    return function (...)
+        local now = tmr.now()
+        local delta = now - last
+        if delta < 0 then delta = delta + 2147483647 end; -- proposed because of delta rolling over, https://github.com/hackhitchin/esp8266-co-uk/issues/2
+        if delta < delay then return end;
+
+        last = now
+        return func(...)
+    end
+end
+
+couter = 0
+
 print("SETTING UP PIN 1 - this is the one I use probably");
 gpio.mode(1, gpio.INT, gpio.PULLUP)
 
 print("DEFINE HANDLER");
 function onChange (level)
     print("LEVEL"..level);
+    print("OPENN TIMES "..couter)
+    couter = couter + 1
     print("PIN IS GETING SIGNAL");
     print("IT WORKS");
 end

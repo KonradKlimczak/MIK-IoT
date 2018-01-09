2fe80b318a5b54d0313f3fa899ccc7d49f3564d4
Konrad <konradklimczak93@gmail.com>
2018-01-09 20:22:36 +0100

    utils added.

--- init.lua
+++ init.lua
@@ -1,23 +1,35 @@
-wifi.setmode(wifi.STATION)
+local utils = require 'utils';
+local secrets = require 'secrets';
 
+print("Hello Konrad :)");
+
+print("Setting up WiFi to Station mode.");
+wifi.setmode(wifi.STATION);
+
+print("Setting configuration object for WiFi connection");
 station_cfg={};
-station_cfg.ssid="networkid";
-station_cfg.pwd="password";
-
+station_cfg.ssid=secrets.ID;
+station_cfg.pwd=secrets.PASS;
 function showip(params)
-    print("Connected to Wifi.  Got IP: " .. params.IP)
-    http.get("http://192.168.1.107:3000/sock/matex2", nil, function(code, data)
-        if (code < 0) then
-          print("HTTP request failed")
-        else
-          print(code, data)
-        end
-      end)
+    print("Connected to Wifi. Got IP: " .. params.IP);
+    gpio.trig(1, "both", utils.debounce(onChange));
+end
+station_cfg.got_ip_cb = showip;
+
+function onChange(level)
+  print("SENT");
+  http.post("http://192.168.1.107:3000/sock/matex2", nil, function(code, data)
+    if (code < 0) then
+      print("HTTP request failed")
+    else
+      print(code, data)
+    end
+  end)
 end
 
-station_cfg.got_ip_cb = showip
-wifi.sta.config(station_cfg)
-tmr.delay(1000000)   -- wait 1,000,000 us = 1 second
-wifi.sta.connect()
-tmr.delay(1000000)   -- wait 1,000,000 us = 1 second
-status_of_wifi = wifi.sta.status()
+print("Setting up mode for PIN 1.");
+gpio.mode(1, gpio.INT, gpio.PULLUP);
+
+print("Connecting to WiFi");
+wifi.sta.config(station_cfg);
+wifi.sta.connect();
---
+++ utils.lua
@@ -0,0 +1,18 @@
+local utils = {}
+
+function utils.debounce(func)
+    local last = 0
+    local delay = 30 * 1000 * 1000 -- 1000ms * 1000 as tmr.now() has μs resolution
+
+    return function (...)
+        local now = tmr.now()
+        local delta = now - last
+        if delta < 0 then delta = delta + 2147483647 end; -- proposed because of delta rolling over, https://github.com/hackhitchin/esp8266-co-uk/issues/2
+        if delta < delay then return end;
+
+        last = now
+        return func(...)
+    end
+end
+
+return utils
